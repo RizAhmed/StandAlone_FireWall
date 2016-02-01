8483eb0afcc389e80b1c7210796fc2ecc8d28a49
Riz Ahmed <rizwan_ahmed21@hotmail.com>
2016-02-01 14:50:35 -0800

    changed user vars for network ips and cards

--- firewall.sh
+++ firewall.sh
@@ -2,8 +2,18 @@
 
 #user configurable section
 IPT="iptables"
+<<<<<<< HEAD
+#firewall interface
+FIREWALL_IF="eno1"
+#host interface
+HOST_IF="enp3s2"
+IP_HOST="192.168.10.100"
+IP_FIREWALL"192.168.10.13"
+
+=======
 ETH0="eth0"
 ETH1="eth1"
+>>>>>>> 0f061320698946596e6c50b8a50391f0358f0c90
 LB_INTERFACE="lo"
 LOOPBACK_IP="127.0.0.1"
 
@@ -40,4 +50,25 @@ then
   exit 0
 fi
 
+<<<<<<< HEAD
 #firewall implementation section
+
+# set default policy to DROP
+$IPT --policy INPUT DROP
+$IPT --policy OUTPUT DROP
+$IPT --policy FORWARD DROP
+
+#inbound/outbound TCP packets on allowed ports
+
+
+#inbound/outbound UDP packets on allowed ports
+
+#inbound/outbound ICMP packets based on type numbers
+
+#drop all packets destined for the firewall host from outside
+
+#do not accept any packets with a source address from outside matching
+#your internal network
+=======
+#firewall implementation section
+>>>>>>> 0f061320698946596e6c50b8a50391f0358f0c90
